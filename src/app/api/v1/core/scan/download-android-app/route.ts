a10dcade79410688d888402fe8b0bd67af891aca
Susant Paudel <techbizznepal@gmail.com>
2025-03-29 06:29:08 +0545

    some refactor

--- src/app/api/v1/core/scan/download-android-app/route.ts
+++ src/app/api/v1/core/scan/download-android-app/route.ts
@@ -1,18 +1,21 @@
 import * as process from "node:process";
 
-const apiUrl = process.env.API_URL || "http://local-ne.techbizz.local/api/v1";
+const apiUrl =
+  process.env.API_URL || "http://local-ne.larashops.local:8081/api/v1";
 const appKey = process.env.APP_KEY || "";
 
 export async function GET() {
   try {
     const res = await fetch(apiUrl.concat("/core/scan/download-android-app"), {
       headers: {
+        "User-Agent": "BazzarifyConsumer",
         "Content-Type": "application/json",
         "X-APP-Key": appKey,
       },
     });
     const data = await res.json();
-    if (!data.metaData.error) {
+
+    if (!data?.metaData?.error) {
       return Response.redirect(data.data.payload["redirectUrl"]);
     }
     return Response.json({ error: data.metaData.error });
