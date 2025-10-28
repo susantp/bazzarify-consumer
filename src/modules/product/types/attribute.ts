3b9c46ab996a8933456fbacc4479b90a8cf45941
Susant Paudel <techbizznepal@gmail.com>
2025-10-28 20:15:05 +0545

    bug fix

--- src/modules/product/types/attribute.ts
+++ src/modules/product/types/attribute.ts
@@ -1,7 +1,7 @@
-import { TAttributeValue } from "@/modules/product/types/attributeValue";
+import { IAttributeValue } from "@/modules/product/types/attributeValue";
 
 export interface IAttribute {
   uuid: string;
   name: string;
-  attribute_value: TAttributeValue[];
+  attribute_value: IAttributeValue[];
 }
