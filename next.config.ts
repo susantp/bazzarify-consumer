947627b901227ecb747cc45e9f36eebaf49c62b9
Susant Paudel <techbizznepal@gmail.com>
2025-10-31 22:38:50 +0545

    add origin

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedDevOrigins: ["10.0.2.2", "consumer.bazzarify.local", "consumer.bazzarify.com", "bazzarify.local", "www.bazzarify.local"],
+  allowedDevOrigins: ["10.0.2.2", "consumer.bazzarify.local", "consumer.bazzarify.com", "bazzarify.local", "bazzarify.com", "www.bazzarify.local", "www.bazzarify.com"],
     typedRoutes: true,
 };
 
